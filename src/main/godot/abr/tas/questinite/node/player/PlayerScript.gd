7e98312dd67d5c1af6074a77137abf1b074400ba
abrart4 <abrart4@nycstudents.net>
2026-07-22 13:49:58 -0400

    the environment is better!

--- src/main/godot/abr/tas/questinite/node/player/PlayerScript.gd
+++ src/main/godot/abr/tas/questinite/node/player/PlayerScript.gd
@@ -6,13 +6,16 @@ const JUMP_VELOCITY = -1600.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		$AnimatedSprite2D.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("ui_accept") && is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.play("jump")
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
