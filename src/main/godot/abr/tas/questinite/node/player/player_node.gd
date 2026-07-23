941e9b94136ced3d04f5e70b3af810563d4670c1
abrart4 <abrart4@nycstudents.net>
2026-07-23 07:01:14 -0400

    Add evil body

--- src/main/godot/abr/tas/questinite/node/player/player_node.gd
+++ src/main/godot/abr/tas/questinite/node/player/player_node.gd
@@ -4,3 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
+
+func _on_evil_cutscene_area_body_entered(body: Node2D) -> void:
+	print(body.name)
